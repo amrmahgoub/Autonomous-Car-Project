a46535fc871e6a2eb33a48bb4b14b0bf856846ac
Amr Mahgoub <90416514+amrmahgoub@users.noreply.github.com>
2024-05-24 04:29:47 +0300

    Add files via upload

---
+++ Layered Arch/STD_LIB/STD.h
@@ -0,0 +1,25 @@
+/*
+ * STD.h
+ *
+ *  Created on: Feb 2, 2022
+ *      Author: Amr Mahgoub
+ */
+
+#ifndef STD_H_
+#define STD_H_
+typedef unsigned char u8;
+typedef signed char s8;
+typedef unsigned short int u16;
+typedef unsigned long int u32;
+typedef signed short int s16;
+typedef signed long int s32;
+typedef float f32;
+typedef double f64;
+typedef enum{
+	FALSE,
+	TRUE
+}boolean;
+
+
+
+#endif /* STD_H_ */
